68488fe4117f44761926285b51c6d347d8945d2d
HimanshuSharma789 <samitihs4@gmail.com>
2019-02-25 14:17:35 +0530

    progress on peer tab

--- phase2/test_new.py
+++ phase2/test_new.py
@@ -25,10 +25,101 @@ class ssh_tab(QWidget):
     def display(self):
         import subprocess
         self.text_area.appendPlainText('>>>> ' + self.cmd_text.text())
-        returned_output = subprocess.check_output(self.cmd_text.text())
-        print('>>>>', returned_output.decode("utf-8"))
-        self.text_area.appendPlainText(returned_output.decode("utf-8"))
+        try:
+        	returned_output = subprocess.check_output(self.cmd_text.text())
+        	print('>>>>', returned_output.decode("utf-8"))
+        	self.text_area.appendPlainText(returned_output.decode("utf-8"))
+        except:
+        	print('something went wrong')
+
         
+class peer_tab(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        # specify layout (horizontal* or vertical)
+        self.peer_layout = QVBoxLayout()
+
+
+        self.browse_layout = QHBoxLayout()
+        # file label
+        self.file_label = QLabel()
+        self.file_label.setText("File: ")
+        # browse textBox
+        self.browse_textBox = QLineEdit(self)
+        # browse button
+        self.browse_btn = QPushButton("Browse")
+        # event to be triggered
+        self.browse_btn.clicked.connect(self.openDialog)
+        # adding the above widget to the layout (tab1)
+        self.browse_layout.addWidget(self.file_label)
+        self.browse_layout.addWidget(self.browse_textBox)
+        self.browse_layout.addWidget(self.browse_btn)
+        self.peer_layout.addLayout(self.browse_layout)
+
+
+        self.port_textBox = QLineEdit()
+        self.port_textBox.setPlaceholderText("Port ")
+        self.username_textBox = QLineEdit()
+        self.username_textBox.setPlaceholderText("Username ")
+        self.address_textBox = QLineEdit()
+        self.address_textBox.setPlaceholderText("Address ")
+        self.pass_textBox = QLineEdit()
+        self.pass_textBox.setPlaceholderText("Password ")
+        self.peer_layout.addWidget(self.port_textBox)
+        self.peer_layout.addWidget(self.username_textBox)
+        self.peer_layout.addWidget(self.address_textBox)
+        self.peer_layout.addWidget(self.pass_textBox)
+        # self.send_layout = QHBoxLayout()
+        # self.send_layout.addStretch()
+        # self.send_btn.setProperty('background-position', left)
+        self.send_btn = QPushButton("Send")
+        self.send_btn.setMaximumWidth(100)
+        self.send_btn.clicked.connect(self.send_files)
+
+
+        self.peer_layout.addWidget(self.send_btn)
+        self.setLayout(self.peer_layout)
+
+    # open the dialog box and return the file
+    def openDialog(self):
+        dlg = QFileDialog()
+        if self.dir_radio.isChecked():
+            dlg.setFileMode(QFileDialog.Directory)
+        else:
+            dlg.setFileMode(QFileDialog.ExistingFiles)
+        if dlg.exec_():
+            self.filenames = dlg.selectedFiles()
+            self.browse_textBox.setText(",".join(self.filenames))
+            print(self.filenames[0])
+            return self.filenames
+
+    def send_files(self):
+        import subprocess
+        # self.text_area.appendPlainText('>>>> ' + self.cmd_text.text())
+        cmd = "ssh -p %s %s@%s" % (self.port_textBox.text(), self.username_textBox.text(), self.address_textBox.text())
+        # print(cmd)
+        try:
+            returned_output = subprocess.check_output(cmd)
+            print('>>>>', returned_output.decode("utf-8"))
+        except subprocess.CalledProcessError:
+            print("error11")            
+        except:
+            print("error12")
+
+        try:
+            returned_output = subprocess.check_output(self.pass_textBox.text())
+            print('>>>>', returned_output.decode("utf-8"))
+        except subprocess.CalledProcessError:
+            print("error21")            
+        except:
+            print("error22")
+
+        # self.text_area.appendPlainText(returned_output.decode("utf-8"))
+
+        # print(opera.scp_query())
+        # cmd = "scp -P %s %s u0_a191@%s:~/ " % (self.ip_add_port_text, self.filenames, self.ip_add_text)
+        # os.system(cmd)
 
 
 '''
@@ -147,9 +238,10 @@ class Application(QWidget):
 
         self.tabs.addTab(broadcast_tab(opera), "Broadcast")
         
-        self.peer_tab = QWidget(parent=None)
-        self.tabs.addTab(self.peer_tab, "P2P")
+        # self.peer_tab = QWidget(parent=None)
+        # self.tabs.addTab(self.peer_tab, "P2P")
         
+        self.tabs.addTab(peer_tab(), "P2P")
         self.tabs.addTab(ssh_tab(), "SSH")
 
 
